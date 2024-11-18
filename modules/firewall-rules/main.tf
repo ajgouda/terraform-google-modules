2bba5dfec0b1279d35babcb1abc7b3e31779d4d7
Ajay Gouda <ajayvgoud@gmail.com>
2024-11-18 08:24:51 +0530

    Add firewall rules

--- modules/firewall-rules/main.tf
+++ modules/firewall-rules/main.tf
@@ -1,16 +1,34 @@
+
 locals {
-  ingress_rules = { for x in var.ingress_rules: x.name => x}
-  egress_rules = {}
+  ingress_rules = { for rule in var.ingress_rules : rule.name => merge(rule, { direction = "INGRESS" }) }
+  egress_rules  = { for rule in var.egress_rules : rule.name => merge(rule, { direction = "EGRESS" }) }
+  rules_all     = merge(local.ingress_rules, local.egress_rules)
 }
 
 
-resource "google_compute_firewall" "firewall_rules" {
-  
-  for_each = local.ingress_rules
-  name = each.value.name
-  network = var.network_name
-  project = var.project_id
-  
+resource "google_compute_firewall" "rules_ingress_egress" {
+  for_each                = local.rules_all
+  name                    = each.value.name
+  direction               = each.value.direction
+  network                 = var.network_name
+  project                 = var.project_id
+  source_ranges           = lookup(each.value, "source_ranges", null)
+  destination_ranges      = lookup(each.value, "destination_ranges", null)
 
 
+  dynamic "allow" {
+    for_each = lookup(each.value, "allow", [])
+    content {
+      protocol = allow.value.protocol
+      ports    = lookup(allow.value, "ports", null)
+    }
+  }
+
+  dynamic "deny" {
+    for_each = lookup(each.value, "deny", [])
+    content {
+      protocol = deny.value.protocol
+      ports    = lookup(deny.value, "ports", null)
+    }
+  }
 }
